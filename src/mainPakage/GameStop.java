ac0ac8d43d378d723385a1ce8ad49ce56570943d
ciao222222223 <piasersergio0@gmail.com>
2025-02-25 14:04:55 +0100

    25/02/25
    implementato e coretto gli errori

--- src/mainPakage/GameStop.java
+++ src/mainPakage/GameStop.java
@@ -2,4 +2,22 @@ package mainPakage;
 
 public class GameStop extends Negozio{
 
+	@Override
+	public void setPrezziBase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void acquistaProdottiMagazzino(double patrimonio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vendiProdotto(double patrimonio, Prodotto prodotto) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
