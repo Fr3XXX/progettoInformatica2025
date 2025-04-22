3085097f0cf3f0009701c61d9575bce47a27f5b6
ciao222222223 <piasersergio0@gmail.com>
2025-04-22 08:35:26 +0200

    -

--- src/viewPackage/MyFrameNegozio.java
+++ src/viewPackage/MyFrameNegozio.java
@@ -1,5 +1,21 @@
 package viewPackage;
 
-public class MyFrameNegozio {
+import java.awt.Color;
 
-}
+import javax.swing.JFrame;
+
+public class MyFrameNegozio extends JFrame{
+	
+	public MyFrameNegozio() {
+		super("");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setResizable(false);// l'utente non puo modificare la grandezza del frame
+		// window.pack();
+
+		this.setLocationRelativeTo(null);// la finestra apparira al centro dello schermo
+		this.setUndecorated(true);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setBackground(Color.RED);
+		this.setVisible(false);
+	}
+}		
--- src/viewPackage/ViewGioielleria.java
+++ src/viewPackage/ViewGioielleria.java
@@ -2,48 +2,81 @@ package viewPackage;
 
 import java.awt.Color;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 import modelPackage.GameObject;
 
-
-
 public class ViewGioielleria extends GameObject {
-    private JPanel frameGioielleria;
+
     private boolean frameVisible = false;
+    boolean open = false;
     GamePanel gamePanel;
+    MyFrameNegozio frameGioielleria;
+    JButton closeFrame;
     
     public ViewGioielleria(GamePanel gamePanel, String path, int x, int y, int size_x, int size_y) {
         super(gamePanel, path, x, y, size_x, size_y);
         this.gamePanel = gamePanel;
-        createPanel();
+        
     }
-    
-    public void createPanel() {
 
-    }
-    
     @Override
     public void update() {
-        if(InputManager.isMouseOver(this) && InputManager.isMousePressed("left")) {
-        	frameVisible = !frameVisible;
-            getFrameGioielleria().setVisible(frameVisible);
-            
-            if(frameVisible) {
-            	getFrameGioielleria().requestFocusInWindow();
+        if (InputManager.isMouseOver(this) && InputManager.isMousePressed("left") && !open) {
+            frameVisible = true;
+            createFrame();
+            open = true; // evita doppie aperture causate da click multipli
+
+            if (frameVisible) {
+                frameGioielleria.requestFocusInWindow();//mette il focus sulla finestra della gioielleria
             }
         }
+        
+        //System.out.println(gamePanel.hasFocus());
         super.update();
     }
 
-	public JPanel getFrameGioielleria() {
-		return frameGioielleria;
-	}
+    public void createFrame() {
+        frameGioielleria = new MyFrameNegozio();
 
-	public void setFrameGioielleria(JPanel frameGioielleria) {
-		this.frameGioielleria = frameGioielleria;
-	}
+        // Bottone di chiusura
+        closeFrame = new JButton("Exit");
+        closeFrame.setFont(new Font("Arial", Font.BOLD, 20));
+        closeFrame.setForeground(Color.WHITE);
+        closeFrame.setBackground(new Color(220, 20, 60)); // Rosso acceso
+        closeFrame.setFocusPainted(false);
+        closeFrame.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        closeFrame.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-	
+    
+        closeFrame.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                closeFrame.setBackground(new Color(255, 69, 0)); // Rosso arancio al passaggio
+            }
+
+            public void mouseExited(MouseEvent e) {
+                closeFrame.setBackground(new Color(220, 20, 60));
+            }
+
+            public void mouseClicked(MouseEvent e) {
+                ViewGioielleria.this.open = false;
+                frameGioielleria.dispose();
+
+                // Qui restituiamo il focus al gamePanel dopo la chiusura
+                SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
+            }
+        });
+
+        closeFrame.setVisible(true);
+        frameGioielleria.add(closeFrame);
+        frameGioielleria.setLocationRelativeTo(null); // centra la finestra
+        frameGioielleria.setVisible(frameVisible);
+    }
 }
